0707481fecea3141476dd1227305a7fb4cc0f0f9
Kolosowski Arseni <kolosowski.a.r@gmail.com>
2023-12-17 10:57:01 +0100

    Update code style

--- LiveVideoEditor/Sources/Modules/Camera/Presenter/Implementation/Extensions/CameraModulePresenter+CameraServiceDelegateProtocol.swift
+++ LiveVideoEditor/Sources/Modules/Camera/Presenter/Implementation/Extensions/CameraModulePresenter+CameraServiceDelegateProtocol.swift
@@ -3,12 +3,8 @@ import CoreMedia.CMSampleBuffer
 
 extension CameraModulePresenter: CameraServiceDelegateProtocol {
 	
-	func didReceiveVideoOutput(_ buffer: CMSampleBuffer) {
-		
-	}
+	func didReceiveVideoOutput(_ buffer: CMSampleBuffer) {}
 	
-	func didReceiveAudioOutput(_ buffer: CMSampleBuffer) {
-		
-	}
+	func didReceiveAudioOutput(_ buffer: CMSampleBuffer) {}
 	
 }
