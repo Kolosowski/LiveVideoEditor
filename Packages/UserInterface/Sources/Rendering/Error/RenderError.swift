9dc51a08e26e6d7cde93ca1f4e08f98826a885c4
Kolosowski Arseni <kolosowski.a.r@gmail.com>
2024-01-28 13:16:00 +0100

    Remove useless cases from RenderError

--- Packages/UserInterface/Sources/Rendering/Error/RenderError.swift
+++ Packages/UserInterface/Sources/Rendering/Error/RenderError.swift
@@ -3,10 +3,7 @@ import Foundation
 public enum RenderError: Error {
 	case deviceCreate
 	case queueCreate
-	case libraryCreate
-	case commandBufferCreate
-	case encoderCreate
 	case textureCacheCreate
-	case textureCreate
 	case imageBufferCreate
+	case textureCreate
 }
