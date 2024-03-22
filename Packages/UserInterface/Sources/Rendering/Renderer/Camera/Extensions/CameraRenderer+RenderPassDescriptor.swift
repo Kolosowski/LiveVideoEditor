c5ba1ad9e133d88d110dc5bbfb53c63bb7e946ef
Kolosowski Arseni <kolosowski.a.r@gmail.com>
2024-03-22 13:26:29 +0100

    Remove useless lines

--- Packages/UserInterface/Sources/Rendering/Renderer/Camera/Extensions/CameraRenderer+RenderPassDescriptor.swift
+++ Packages/UserInterface/Sources/Rendering/Renderer/Camera/Extensions/CameraRenderer+RenderPassDescriptor.swift
@@ -4,7 +4,7 @@ extension CameraRenderer {
 	
 	func renderPassDescriptor(_ texture: MTLTexture?) -> MTLRenderPassDescriptor {
 		let passDescriptor = MTLRenderPassDescriptor()
-		passDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0.5, 1, 1, 1)
+		passDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0, 0, 0, 1)
 		passDescriptor.colorAttachments[0].texture = texture
 		return passDescriptor
 	}
