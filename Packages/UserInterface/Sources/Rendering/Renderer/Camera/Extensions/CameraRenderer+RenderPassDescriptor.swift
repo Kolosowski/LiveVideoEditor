824996fbbb5d19a749854d491c73a75b553c5152
Kolosowski Arseni <kolosowski.a.r@gmail.com>
2024-03-22 12:52:49 +0100

    Update render pass descriptor create method

--- Packages/UserInterface/Sources/Rendering/Renderer/Camera/Extensions/CameraRenderer+RenderPassDescriptor.swift
+++ Packages/UserInterface/Sources/Rendering/Renderer/Camera/Extensions/CameraRenderer+RenderPassDescriptor.swift
@@ -4,9 +4,7 @@ extension CameraRenderer {
 	
 	func renderPassDescriptor(_ texture: MTLTexture?) -> MTLRenderPassDescriptor {
 		let passDescriptor = MTLRenderPassDescriptor()
-		passDescriptor.colorAttachments[0].loadAction = .clear
-		passDescriptor.colorAttachments[0].storeAction = .store
-		passDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0, 1, 1, 1)
+		passDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0.5, 1, 1, 1)
 		passDescriptor.colorAttachments[0].texture = texture
 		return passDescriptor
 	}
